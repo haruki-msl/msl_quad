3128c44e224d5f2c49ef58410ca3ed09533c9667
Haruki Nishimura <hnishimura@stanford.edu>
2019-02-08 16:16:57 -0800

    Initial commit to simple_waypoint_following branch

---
+++ mslquad/src/pose_track_waypoint_publisher_node.cpp
@@ -0,0 +1,34 @@
+/**************************************************************************
+  File Name    : pose_track_waypoint_publisher_node.cpp
+  Author       : Haruki Nishimura
+                 Multi-Robot Systems Lab (MSL), Stanford University
+  Contact      : hnishimura@stanford.edu
+  Create Time  : Jan 29, 2018.
+  Description  : ROS node for waypoint following with pose_track controller.
+**************************************************************************/
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "mslquad/waypoint_publisher.h"
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "pose_track_waypoint_publisher");
+
+    // Check if waypoints file is specified.
+    ros::V_string args;
+    ros::removeROSArgs(argc, argv, args);
+    if (args.size() != 3) {
+        ROS_ERROR("Usage: pose_track_waypoint_publisher_node <quad_ns> <waypoint_file> \n"
+                  "Each line should read waiting_time_s x y z yaw_deg");
+        return -1;
+    }
+    std::string quad_ns = args.at(1);
+    std::string waypoints_file_path = args.at(2);
+
+    PX4WaypointPublisher waypoint_publisher(quad_ns, waypoints_file_path);
+    ROS_INFO("Waypoint_publisher node initiated.");
+    ros::spin();
+    return 0;
+}
